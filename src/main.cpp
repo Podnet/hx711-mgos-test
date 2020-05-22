110595a872bb7ccea7893e3c05ffe24e0fc0a6e3
hrithik098 <hrithiky328@gmail.com>
2020-05-23 03:14:53 +0530

    Second commit : Changed main.c to .cpp works like charm

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,23 @@
 #include "mgos_hx711.h"
 
 #include "mgos.h"
+struct HX711_ADC *hx;
 
 
 
-
-// void read_values(){
-//   float value = mgos_hx711_get_data();
-//   LOG(LL_INFO, ("TCU: Load cell value : %.2f", value));
-// }
+void read_values(){
+  float value = mgos_hx711_get_data(hx);
+  LOG(LL_INFO, ("TCU: Load cell value : %.2f", value));
+}
 extern "C"{
 enum mgos_app_init_result mgos_app_init(void) {
   LOG(LL_INFO, ("TCU: Fetching Loadcell readings"));
   mgos_hx711_create(10,13);
-  // mgos_hx711_begin();
-  // mgos_hx711_set_cal_factor(); // Need to be calculated as per our load cell
-  // mgos_hx711_tare(); // Reset the tare to zero
-  // read_values();
+  mgos_hx711_set_gain(hx, 128);
+  mgos_hx711_begin(hx);
+  mgos_hx711_set_cal_factor(hx, 12546); // Need to be calculated as per our load cell
+  mgos_hx711_tare(hx); // Reset the tare to zero
+  read_values();
   return MGOS_APP_INIT_SUCCESS;
 
 }
