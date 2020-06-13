9b486e14b6aa1d265709754050c203a677655074
hrithik098 <hrithiky328@gmail.com>
2020-06-13 12:15:58 +0530

    Testing header files

---
+++ include/mgos_hx711_app.h
@@ -0,0 +1,7 @@
+#include "mgos_hx711.h"
+#include "mgos.h"
+
+
+
+enum mgos_app_init_result mgos_app_init(void);
+void read_values_cb(void *arg);
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "mgos_hx711.h"
 #include "mgos.h"
-struct HX711_ADC *hx; 
+struct HX711_ADC *hx;
 
 /*! \mainpage Hx711 testing
  *
@@ -9,36 +9,35 @@ struct HX711_ADC *hx;
  * This is a test app to read local cell values using HX711.
  */
 
-void read_values(void *arg)
+void read_values_cb(void *arg)
 {
 
     if (mgos_hx711_update(hx))
     {
-        float reading = mgos_hx711_get_data(hx); // Get reading from loadcell 
+        float reading = mgos_hx711_get_data(hx); // Get reading from loadcell
         LOG(LL_INFO, ("TCU: Load cell: Reading %.2f", reading));
     }
 
     else
     {
         LOG(LL_INFO, ("TCU: Load cell: mgos_hx711_update function returned false."));
-        mgos_hx711_refresh_data_set(hx);  // Refresh the data buffered 
+        mgos_hx711_refresh_data_set(hx); // Refresh the data buffered
     }
 }
 /**<  Function to read values from Load cells */
-
 
 extern "C"
 {
     enum mgos_app_init_result mgos_app_init(void)
     {
         LOG(LL_INFO, ("TCU: Fetching Loadcell readings"));
-        hx = mgos_hx711_create(15, 13); // HX711 class object  
-        mgos_hx711_set_gain(hx, 128); // Set appropriate gain from loadcell readings 
-        mgos_hx711_begin(hx);    // Begin conversion of loadcell to hx711 values 
-        mgos_hx711_set_cal_factor(hx, 2230); // Need to be calculated as per our load cell 
-        mgos_hx711_tare(hx); // Reset the values to zero again 
+        hx = mgos_hx711_create(15, 13);      // HX711 class object
+        mgos_hx711_set_gain(hx, 128);        // Set appropriate gain from loadcell readings
+        mgos_hx711_begin(hx);                // Begin conversion of loadcell to hx711 values
+        mgos_hx711_set_cal_factor(hx, 2230); // Need to be calculated as per our load cell
+        mgos_hx711_tare(hx);                 // Reset the values to zero again
 
-        mgos_set_timer(3000, MGOS_TIMER_REPEAT, read_values, NULL); // Getting loadcell values every 3000ms 
+        mgos_set_timer(3000, MGOS_TIMER_REPEAT, read_values_cb, hx); // Getting loadcell values every 3000ms
 
         return MGOS_APP_INIT_SUCCESS;
     }
